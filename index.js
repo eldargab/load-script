d200c867f5ce55f59d5a56b4fcc4ca174829fbe2
jsdevel <js.developer.undefined@gmail.com>
2015-03-02 10:42:36 -0700

    Adding options.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 
-module.exports = function load (src, cb) {
+module.exports = function load (src, opts, cb) {
   var head = document.head || document.getElementsByTagName('head')[0]
   var script = document.createElement('script')
 
-  cb = cb || function() {};
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
 
-  script.type = 'text/javascript'
-  script.charset = 'utf8'
-  script.async = true
+  opts = opts || {}
+  cb = cb || function() {}
+
+  script.type = opts.type || 'text/javascript'
+  script.charset = opts.charset || 'utf8';
+  script.async = 'async' in opts ? !!opts.async : true
   script.src = src
+
+  if (opts.attrs) {
+    setAttributes(script, opts.attrs)
+  }
+
+  if (opts.text) {
+    script.appendChild(document.createTextNode(opts.text))
+  }
 
   var onend = 'onload' in script ? stdOnEnd : ieOnEnd
   onend(script, cb)
@@ -23,16 +37,22 @@ module.exports = function load (src, cb) {
   head.appendChild(script)
 }
 
+function setAttributes(script, attrs) {
+  for (var attr in attrs) {
+    script.setAttribute(attr, attrs[attr]);
+  }
+}
+
 function stdOnEnd (script, cb) {
   script.onload = function () {
     this.onerror = this.onload = null
-    cb()
+    cb(null, script)
   }
   script.onerror = function () {
     // this.onload = null here is necessary
     // because even IE9 works not like others
     this.onerror = this.onload = null
-    cb(new Error('Failed to load ' + this.src))
+    cb(new Error('Failed to load ' + this.src), script)
   }
 }
 
@@ -40,6 +60,6 @@ function ieOnEnd (script, cb) {
   script.onreadystatechange = function () {
     if (this.readyState != 'complete' && this.readyState != 'loaded') return
     this.onreadystatechange = null
-    cb(null, true) // there is no way to catch loading errors in IE8
+    cb(null, script) // there is no way to catch loading errors in IE8
   }
 }
